12b18273abcee5e481663332945ae09e2fa3cf02
Lucian Iernye <lucian.iernye@gmail.com>
2020-03-30 21:08:24 +0100

    files renamed

--- author.php
+++ author.php
@@ -13,14 +13,14 @@ $numeAutor = mysqli_result ($resursaNumeAutor, 0, "nume_autor");
 <td style="border-color:#CCCCCC; background-color:#E6F3FF; padding:4px; border:solid #000066 1px" valign="top" width="767px">
 <?php
 print '<div style="color:#000033; font:Arial, Helvetica, sans-serif; font-size:14px; font-weight:bold">
-<b>Carti de 
+<b>Books by 
 <u><i>'.$numeAutor.'</i></u>:</b>
 </div>';
 ?>
 <table cellpadding="4" border="0">
 <?php
-//$sql = "SELECT * FROM carti, autori, domenii WHERE carti.id_autor=autori.id_autor AND autori.id_autor=".$id_autor['id_autor'];
-$sql = "SELECT * FROM carti WHERE carti.id_autor=".$id_autor;
+$sql = "SELECT * FROM carti, autori, domenii WHERE carti.id_autor=autori.id_autor AND autori.id_autor=".$id_autor['id_autor'];
+// $sql = "SELECT * FROM carti WHERE carti.id_autor=".$id_autor;
 $resursa = mysqli_query(OpenCon(), $sql);
 while($row = mysqli_fetch_array($resursa))
 {
