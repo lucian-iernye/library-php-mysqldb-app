7c9e0989c4956726297f3d5cebfb677b8f840088
Lucian Iernye <lucian.iernye@gmail.com>
2020-04-06 16:09:31 +0100

    table fields changed in the delivery information form

--- shopping_checkout.php
+++ shopping_checkout.php
@@ -38,7 +38,9 @@ print '<tr>
 ?>
 </table>
 <br /><br />
-<div align="center" style="color:#000033; font:Arial, Helvetica, sans-serif; font-size:14px; font-weight:bold">Delivery informations</div><br />
+
+<div align="center" style="color:#000033; font:Arial, Helvetica, sans-serif; font-size:14px; font-weight:bold">Delivery informations:
+</div><br />
 <form action="processing.php" method="post">
 <table align="center" border="0" style="border-color:#CCCCCC; background-color:#E6F3FF; padding:4px; border:solid #000066 1px" width="600px">
 <tr>
@@ -46,172 +48,128 @@ print '<tr>
 <!--aici tabel-->
 <table border="0">
 <tr align="right">
-<td><b>First Name<font color="#FF0000">*</font>:</b></td>
+<td><b>First Name:<font color="#FF0000">*</font>:</b></td>
 <td><input type="text" name="first-name" /></td>
 </tr>
 
 <tr align="right">
-<td><b>Phone number<font color="#FF0000">*</font>:</b></td>
-<td><input type="text" name="phone-number" /></td>
-</tr>
-
-<tr align="right">
-<td><b>Email:</b></td>
-<td><input type="text" name="email" /></td>
-</tr>
-<!--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
-<tr align="right">
-<td><b>City<font color="#FF0000">*</font>:</b></td>
-<td>
-<select name="judet">
-
-    <option value="judet" selected="selected">Select a city:</option>
-
-    <option value="judet">Alba</option>
-
-    <option value="judet">Arad</option>
-
-
-    <option value="judet">Arges</option>
-
-    <option value="judet">Bacau</option>
-
-    <option value="judet">Bihor</option>
-
-    <option value="judet">Bistrita-Nasaud</option>
-
-    <option value="judet">Botosani</option>
-
-    <option value="judet">Braila</option>
-
-    <option value="judet">Brasov</option>
-
-    <option value="judet">Buzau</option>
-
-    <option value="judet">Caras Severin</option>
-
-    <option value="judet">Calarasi</option>
-
-    <option value="judet">Cluj</option>
-
-    <option value="judet">Constanta</option>
-
-    <option value="judet">Covasna</option>
-
-    <option value="judet">Dambovita</option>
-
-    <option value="judet">Dolj</option>
-
-    <option value="judet">Galati</option>
-
-    <option value="judet">Giurgiu</option>
-
-    <option value="judet">Gorj</option>
-
-    <option value="judet">Harghita</option>
-
-    <option value="judet">Hunedoara</option>
-
-    <option value="judet">Ialomita</option>
-
-    <option value="judet">Iasi</option>
-
-    <option value="judet">Ilfov</option>
-
-    <option value="judet">Maramures</option>
-
-    <option value="judet">Mehedinti</option>
-
-    <option value="judet">Mures</option>
-
-    <option value="judet">Neamt</option>
-
-    <option value="judet">Olt</option>
-
-    <option value="judet">Prahova</option>
-
-    <option value="judet">Satu Mare</option>
-
-    <option value="judet">Salaj</option>
-
-    <option value="judet">Sibiu</option>
-
-    <option value="judet">Suceava</option>
-
-    <option value="judet">Teleorman</option>
-
-    <option value="judet">Timis</option>
-
-    <option value="judet">Tulcea</option>
-
-    <option value="judet">Vaslui</option>
-
-    <option value="judet">Valcea</option>
-
-    <option value="judet">Vrancea</option>
-
-  </select>
-</td>
-</tr>
-<!--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------->
-<tr align="right">
-<td><b>Number<font color="#FF0000">*</font>:</b></td>
-<td><input type="text" name="numar" /></td>
-</tr>
-
-<!-- <tr align="right">
-<td><b>Scara:</b></td>
-<td><input type="text" name="scara" /></td>
-</tr> -->
-
-<tr align="right">
-<td><b>Apart.:</b></td>
-<td><input type="text" name="apart" /></td>
-</tr>
-
-
-</table>
-<!--aici tabel-->
-</td>
-<td>
-<!--aici tabel-->
-<table border="0">
-<tr align="right">
-<td><b>Last Name<font color="#FF0000">*</font>:</b></td>
+<td><b>Last Name:<font color="#FF0000">*</font>:</b></td>
 <td><input type="text" name="last-name" /></td>
 </tr>
 
 <tr align="right">
-<td><b>Phone<font color="#FF0000">*</font>:</b></td>
-<td><input type="text" name="telefon" /></td>
+<td><b>Email:<font color="#FF0000">*</font></b></td>
+<td><input type="text" name="email" /></td>
 </tr>
 
 <tr align="right">
-<td><b>County<font color="#FF0000">*</font>:</b></td>
-<td><input type="text" name="localitate" /></td>
-</tr>
-
-<tr align="right">
-<td><b>Street<font color="#FF0000">*</font>:</b></td>
-<td><input type="text" name="strada" /></td>
-</tr>
-
-<!-- <tr align="right">
-<td><b>Bloc:</b></td>
-<td><input type="text" name="bloc" /></td>
-</tr>
-
-<tr align="right">
-<td><b>Etaj:</b></td>
-<td><input type="text" name="etaj" /></td>
-</tr> -->
-
-<tr align="right">
-<td><b>Post Code<font color="#FF0000">*</font>:</b></td>
-<td><input type="text" name="cod_postal" /></td>
+<td><b>Phone number:<font color="#FF0000">*</font></b></td>
+<td><input type="text" name="phone" /></td>
 </tr>
 </table>
 <!--aici tabel-->
+</td>
+<td>
+
+
+
+<!--aici tabel-->
+<table border="0">
+<tr align="right">
+<td><b>Address:<font color="#FF0000">*</font>:</b></td>
+<td>
+<select name="city">
+
+    <option value="city" selected="selected">Select a city:</option>
+    <option value="city">Aberdeen</option>
+    <option value="city">Armagh</option>
+    <option value="city">Bangor</option>
+    <option value="city">Bath</option>
+    <option value="city">Belfast</option>
+    <option value="city">Birmingham</option>
+    <option value="city">Bradford</option>
+    <option value="city">Brighton & Hove</option>
+    <option value="city">Bristol</option>
+    <option value="city">Cambridge</option>
+    <option value="city">Canterbury</option>
+    <option value="city">Cardiff</option>
+    <option value="city">Carlisle</option>
+    <option value="city">Chelmsford</option>
+    <option value="city">Chester</option>
+    <option value="city">Chichester</option>
+    <option value="city">Coventry</option>
+    <option value="city">Derby</option>
+    <option value="city">Derry</option>
+    <option value="city">Dundee</option>
+    <option value="city">Durham</option>
+    <option value="city">Edinburgh</option>
+    <option value="city">Ely</option>
+    <option value="city">Exeter</option>
+    <option value="city">Glasgow</option>
+    <option value="city">Gloucester</option>
+    <option value="city">Hereford</option>
+    <option value="city">Inverness</option>
+    <option value="city">Kingston upon Hull</option>
+    <option value="city">Lancaster</option>
+    <option value="city">Leeds</option>
+    <option value="city">Leicester</option>
+    <option value="city">Lichfield</option>
+    <option value="city">Lincoln</option>
+    <option value="city">Lisburn</option>
+    <option value="city">Liverpool</option>
+    <option value="city">London</option>
+    <option value="city">Manchester</option>
+    <option value="city">Newcastle upon Tyne</option>
+    <option value="city">Newport</option>
+    <option value="city">Newry</option>
+    <option value="city">Norwich</option>
+    <option value="city">Nottingham</option>
+    <option value="city">Oxford</option>
+    <option value="city">Perth</option>
+    <option value="city">Peterborough</option>
+    <option value="city">Plymouth</option>
+    <option value="city">Preston</option>
+    <option value="city">Ripon</option>
+    <option value="city">St Albans</option>
+    <option value="city">St Asaph</option>
+    <option value="city">St Davids</option>
+    <option value="city">Salford</option>
+    <option value="city">Salisbury</option>
+    <option value="city">Sheffield</option>
+    <option value="city">Southampton</option>
+    <option value="city">Stirling</option>
+    <option value="city">Stoke-on-Trent</option>
+    <option value="city">Sunderland</option>
+    <option value="city">Swansea</option>
+    <option value="city">Truro</option>
+    <option value="city">Wakefield</option>
+    <option value="city">Wells</option>
+    <option value="city">Westminster</option>
+    <option value="city">Winchester</option>
+    <option value="city">Wolverhampton</option>
+    <option value="city">Worcester</option>
+    <option value="city">York</option>
+  </select>
+</td>
+</tr>
+
+<tr align="right">
+<td><b>Address Line 1<font color="#FF0000">*</font>:</b></td>
+<td><input type="text" name="address-one" /></td>
+</tr>
+
+<tr align="right">
+<td><b>Address Line 2</b></td>
+<td><input type="text" name="scara" /></td>
+</tr>
+
+<tr align="right">
+<td><b>Post Code</b></td>
+<td><input type="text" name="apart" /></td>
+</tr>
+</table>
+<!--aici tabel-->
+
 </td>
 </tr>
 </table>
